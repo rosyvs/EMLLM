a00faace3bb7ec3fd7abbdc08d39bb5a09960560
rosyvs <rosy.southwell@gmail.com>
2025-01-22 17:57:45 -0700

    Add function to load EEG data from .mat files and example usage w ZuCo

--- eeg/get_bendr_feats.py
+++ eeg/get_bendr_feats.py
@@ -5,6 +5,9 @@ from bendr import ConvEncoderBENDR, BENDRContextualizer, BendingCollegeWav2Vec
 import torch
 import tqdm
 import argparse
+from scipy import io
+import h5py
+
 # from dn3.configuratron import ExperimentConfig
 # from dn3.transforms.instance import To1020
 # from dn3.transforms.batch import RandomTemporalCrop
@@ -32,3 +35,23 @@ encoder = ConvEncoderBENDR(args.enc_channels, encoder_h=args.hidden_size)
 contextualizer = BENDRContextualizer(encoder.encoder_h, layer_drop=args.layer_drop)
 encoder.load(encoder_weights)
 contextualizer.load(context_weights)
+
+# load some example EEG data
+# ZuCo
+path_to_zuco = '/Users/roso8920/Emotive Computing Dropbox/Rosy Southwell/EEG-Gaze/ZuCo/osfstorage'
+file_name = os.path.join(path_to_zuco,"task1- SR/Preprocessed/ZPH/gip_ZPH_SNR1_EEG.mat")
+# data = io.loadmat(file_name, squeeze_me=True, struct_as_record=False)
+
+def load_mat_file(filepath):
+    arrays = {}
+    f = h5py.File(filepath)
+    for k, v in f.items():
+        if isinstance(v, h5py.Dataset):
+            try:
+                v = np.array(v)
+            except:
+                pass
+        arrays[k] = v
+    return arrays
+data = load_mat_file(file_name)
+
